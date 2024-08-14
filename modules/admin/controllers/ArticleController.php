8c1d678c5824bc2e58bc40c496565bb21abe1084
GALEGEN <ssstalker00@gmail.com>
2024-08-14 18:13:52 +0300

    setting up tags

--- modules/admin/controllers/ArticleController.php
+++ modules/admin/controllers/ArticleController.php
@@ -11,6 +11,7 @@ use app\models\ImageUpload;
 use yii\web\UploadedFile;
 use app\models\Category;
 use yii\helpers\ArrayHelper;
+use app\models\Tag;
 
 /**
  * ArticleController implements the CRUD actions for Article model.
@@ -170,6 +171,24 @@ class ArticleController extends Controller
             'article' => $article,
             'selectedCategory' => $selectedCategory,
             'categories' => $categories,
+        ]);
+    }
+    
+    public function actionSetTags($id){
+        $article = $this->findModel($id);
+        $selectedTags = $article->getSelectedTags(); //
+        $tags = ArrayHelper::map(Tag::find()->all(), 'id', 'title');
+
+        if(\Yii::$app->request->isPost)
+        {
+            $tags = \Yii::$app->request->post('tags');
+            $article->saveTags($tags);
+            return $this->redirect(['view', 'id'=>$article->id]);
+        }
+        
+        return $this->render('tags', [
+            'selectedTags'=>$selectedTags,
+            'tags'=>$tags
         ]);
     }
 }
